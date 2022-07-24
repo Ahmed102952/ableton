72f408076859f43d88618c88e5fbdf4ad3ee4452
Ahmed102952 <ahmeda8400@gmail.com>
2022-07-24 22:22:44 +0200

    optimaize images

--- src/script/script.ts
+++ src/script/script.ts
@@ -1,5 +1,25 @@
 import { activeMenu } from "./util/activeMenu";
 
+(
+	document.querySelectorAll("[data-poster]") as NodeListOf<HTMLImageElement>
+).forEach((img, i) => {
+	img.src = `https://ik.imagekit.io/b85lgzght1m/ableton/poster-${i + 1}.jpg`;
+	img.srcset = `
+	https://ik.imagekit.io/b85lgzght1m/tr:w-500/ableton/poster-${i + 1}.jpg 500, 
+	https://ik.imagekit.io/b85lgzght1m/tr:w-1000/ableton/poster-${i + 1}.jpg 1000
+	https://ik.imagekit.io/b85lgzght1m/tr:w-1366/ableton/poster-${i + 1}.jpg 1366`;
+});
+
+(
+	document.querySelectorAll(
+		"[data-collage-img]"
+	) as NodeListOf<HTMLDivElement>
+).forEach((div, i) => {
+	div.style.backgroundImage = `url(https://ik.imagekit.io/b85lgzght1m/tr:w-${
+		div.clientWidth
+	}/ableton/photo-${i + 1}.jpg)`;
+});
+
 document.querySelector("[data-menu-btn]")?.addEventListener("click", () => {
 	activeMenu(
 		document.querySelector("[data-menu-btn]") as Element,
