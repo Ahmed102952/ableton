07d8df10bb477c26736c5a220ea17e3357a8238d
Ahmed102952 <ahmeda8400@gmail.com>
2022-07-25 17:23:41 +0200

    ..

--- src/script/script.ts
+++ src/script/script.ts
@@ -1,37 +1,48 @@
 import { activeMenu } from "./util/activeMenu";
 
-(document.querySelector("[data-header-img]") as HTMLDivElement).style.backgroundImage = `url(https://ik.imagekit.io/b85lgzght1m/tr:w-${document.querySelector("[data-header-img]")?.clientWidth}/ableton/header.jpg)`;
+window.addEventListener("DOMContentLoaded", () => {
+	(
+		document.querySelector("[data-header-img]") as HTMLDivElement
+	).style.backgroundImage = `url(https://ik.imagekit.io/b85lgzght1m/tr:w-${
+		(document.querySelector("[data-header-img]")?.clientWidth as number) +
+		400
+	}/ableton/header.jpg)`;
 
-(
-	document.querySelectorAll("[data-poster]") as NodeListOf<HTMLImageElement>
-).forEach((img, i) => {
-	img.src = `https://ik.imagekit.io/b85lgzght1m/ableton/poster-${i + 1}.jpg`;
-	img.srcset = `
-	https://ik.imagekit.io/b85lgzght1m/tr:w-500/ableton/poster-${i + 1}.jpg 500, 
-	https://ik.imagekit.io/b85lgzght1m/tr:w-1000/ableton/poster-${i + 1}.jpg 1000
-	https://ik.imagekit.io/b85lgzght1m/tr:w-1366/ableton/poster-${i + 1}.jpg 1366`;
-});
+	(
+		document.querySelectorAll(
+			"[data-poster]"
+		) as NodeListOf<HTMLImageElement>
+	).forEach((img, i) => {
+		img.src = `https://ik.imagekit.io/b85lgzght1m/ableton/poster-${
+			i + 1
+		}.jpg`;
+		img.srcset = `
+		https://ik.imagekit.io/b85lgzght1m/tr:w-500/ableton/poster-${i + 1}.jpg 500, 
+		https://ik.imagekit.io/b85lgzght1m/tr:w-1000/ableton/poster-${i + 1}.jpg 1000
+		https://ik.imagekit.io/b85lgzght1m/tr:w-1366/ableton/poster-${i + 1}.jpg 1366`;
+	});
 
-(
-	document.querySelectorAll(
-		"[data-collage-img]"
-	) as NodeListOf<HTMLDivElement>
-).forEach((div, i) => {
-	div.style.backgroundImage = `url(https://ik.imagekit.io/b85lgzght1m/tr:w-${
-		div.clientWidth
-	}/ableton/photo-${i + 1}.jpg)`;
-});
+	(
+		document.querySelectorAll(
+			"[data-collage-img]"
+		) as NodeListOf<HTMLDivElement>
+	).forEach((div, i) => {
+		div.style.backgroundImage = `url(https://ik.imagekit.io/b85lgzght1m/tr:w-${
+			div.clientWidth
+		}/ableton/photo-${i + 1}.jpg)`;
+	});
 
-document.querySelector("[data-menu-btn]")?.addEventListener("click", () => {
-	activeMenu(
-		document.querySelector("[data-menu-btn]") as Element,
-		document.querySelector("[data-menu]") as Element
-	);
-});
+	document.querySelector("[data-menu-btn]")?.addEventListener("click", () => {
+		activeMenu(
+			document.querySelector("[data-menu-btn]") as Element,
+			document.querySelector("[data-menu]") as Element
+		);
+	});
 
-document.querySelector("[data-more-btn]")?.addEventListener("click", () => {
-	activeMenu(
-		document.querySelector("[data-more-btn]") as Element,
-		document.querySelector("[data-desktop-menu]") as Element
-	);
+	document.querySelector("[data-more-btn]")?.addEventListener("click", () => {
+		activeMenu(
+			document.querySelector("[data-more-btn]") as Element,
+			document.querySelector("[data-desktop-menu]") as Element
+		);
+	});
 });
